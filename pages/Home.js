216c8b46086b4cf61ae5699b3bec0908acf65889
lokeshbsr <32267176+lokeshbsr@users.noreply.github.com>
2023-04-07 15:48:43 +0530

    router new features description added

--- pages/Home.js
+++ pages/Home.js
@@ -3,6 +3,38 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>Welcome to the New Router6.10! features</h1>
+    1.Data Fetching (loader, actions for Form)
+    <br />
+    2.ceating routes as an objects
+    <br />
+    3.Don't need 'Routes' component only Route is required will have a root
+    'Outlet'
+    <br />
+    4.Routing Structure depends on the developer.
+    <p>
+      ***********************************************************************************
+    </p>
+    <div>
+      <strong>Our Current React Version:</strong>
+      <br />
+      react-router-dom@6.2.2
+      <br />
+      react@17.0.2
+      <br />
+      react-dom@17.0.2
+      <br />
+    </div>
+    <br />
+    <div>
+      <strong>Latest React Version:</strong>
+      <br />
+      react-router-dom@6.10.0
+      <br />
+      react@18.2.0
+      <br />
+      react-dom@18.2.0
+      <br />
+    </div>
   </div>
 );
 
